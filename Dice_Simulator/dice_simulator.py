d79a1901c201a01b36b2b05d3b4b4d81c3e98af8
AlexDolch <117640588+AlexDolch@users.noreply.github.com>
2023-06-19 14:38:49 +0200

    Add files via upload

---
+++ Dice_Simulator/dice_simulator.py
@@ -0,0 +1,32 @@
+
+import random
+
+
+def roll_dice(amount: int = 2) -> list[int]:
+    if amount <= 0:
+        raise ValueError
+    
+    rolls: list[int] = []
+    for i in range(amount):
+        random_roll: int = random.randint(1, 6)
+        rolls.append(random_roll)
+        
+    return rolls
+
+
+def main():
+    while True:
+        try:
+            user_input: str = input(" How many dice would u like to roll? ")
+            
+            if user_input.lower() == "exit":
+                print("Thanx for rolling dice!")
+                break
+            
+            print(*roll_dice(int(user_input)), sep=", ")
+        except ValueError: 
+            print("(Please enter a valid number)")
+
+
+if __name__ == "__main__":
+    main()
