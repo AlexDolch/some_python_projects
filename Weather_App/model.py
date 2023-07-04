38c3d25c35909050f7ff4ca4a1c755f388150886
AlexDolch <117640588+AlexDolch@users.noreply.github.com>
2023-07-04 16:57:40 +0200

    Add files via upload

---
+++ Weather_App/model.py
@@ -0,0 +1,16 @@
+
+from dataclasses import dataclass
+from datetime import datetime as dt
+
+
+@dataclass
+class Weather:
+    date: dt
+    details: dict
+    temp: str
+    weather: list[dict]
+    description: str
+    
+    def __str__(self):
+        return f"[{self.date:%H:%M}] {self.temp}C° ({self.description})"
+        
